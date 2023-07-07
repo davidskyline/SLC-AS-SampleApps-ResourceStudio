0eae0ca4c8fc98bd8c5645415368a19ed14673c0
Jens Vandewalle <jens.vandewalle@skyline.be>
2023-07-07 13:01:59 +0200

    Added data source for retrieving property values
    Change-Id: I4f93ade5480dba4d6a21b78f7567e6f84885bda2

---
+++ ResourceManagement_GQI_Get Property Values_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ResourceManagement_GQI_Get Property Values_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("ResourceManagement_GQI_Get Property Values_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("5971B4D3-0699-4B3F-99EF-EC84691E4103")]
+[assembly: AssemblyVersion("1.0.0.0")]
